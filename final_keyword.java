429350da2dd6799dc4020842c2513bcb7d3910c7
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-14 18:56:40 +0530

    package and this implemented

--- final_keyword.java
+++ final_keyword.java
@@ -1,10 +1,10 @@
 class final_keyword{  
-    final int speedlimit=90;//final variable  
-    void run(){  
-     speedlimit=400;  
-    }  
-    public static void main(String args[]){  
+    final int speedlimit=90; //final variable  
+    //void run(){  
+    speedlimit=400;  
+    //}  
+    public static void main(String args[]){ 
     final_keyword obj=new  final_keyword();  
-    obj.run();  
+    //obj.run();  
     }  
    }//end of class  
---
+++ mypackage/Package.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class Package {
+    public static void myMethod()
+    {
+        System.out.println("this is my method");
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to mypackage");
+    }
+}
