7262dba0aa5533eb240870b208b74ae8b3836cea
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-14 15:53:14 +0530

    final keyword

---
+++ final_keyword.java
@@ -0,0 +1,10 @@
+class final_keyword{  
+    final int speedlimit=90;//final variable  
+    void run(){  
+     speedlimit=400;  
+    }  
+    public static void main(String args[]){  
+    final_keyword obj=new  final_keyword();  
+    obj.run();  
+    }  
+   }//end of class  
