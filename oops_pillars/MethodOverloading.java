67849ac1ac7cefa31e9e7ce153e4661d2d2675da
Mir Rafath Ali <mirrafathali100@gmail.com>
2022-01-16 20:16:34 +0530

    method overloading in Java

---
+++ oops_pillars/MethodOverloading.java
@@ -0,0 +1,20 @@
+//If a class has multiple methods having same name but different in parameters, it is known as Method Overloading.
+class Adder
+{  
+    static int add(int a,int b)
+    {
+        return a+b;
+    }  
+    static int add(int a,int b,int c)
+    {
+        return a+b+c;
+    }  
+}  
+class MethodOverloading
+{  
+    public static void main(String[] args)
+    {  
+        System.out.println(Adder.add(11,11));  
+        System.out.println(Adder.add(11,11,11));  
+    }
+}  
